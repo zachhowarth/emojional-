79598d6c40b89b85f4a233039108c75d94319ef5
Zach Howarth <zachhowarth@gmail.com>
2019-05-11 12:17:32 -0500

    Complete Iteration 3

--- Dinosaur or Unicorn?/ViewController.swift
+++ Dinosaur or Unicorn?/ViewController.swift
@@ -9,23 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     let emojis = [ "🦖" : "Ur a heckin' dinosaur" , "🦄" : "Like, ur a fabulous unicorn"]
+    let customMessages = [
+        "Ur a heckin' dinosaur": ["RAWR! Ur a dino brah" , "Sup dino?" , "DINO GANG" ],
+        "Like, ur a fabulous unicorn": ["YAS QUEEN. Keep calm and unicorn on" , "UNICORNS...ASSEMBLE!" , "Yay! Ur a unicorn brah"]
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
     @IBAction func showMessage(sender: UIButton) {
-        
         let selectedEmotion = sender.titleLabel?.text
         
+        let emotionArray = customMessages[emojis[selectedEmotion!]!]
+        let random = Int.random(in: 0 ..< emotionArray!.count)
+        let emojiMessage = emotionArray?[random]
         
+        let alertController = UIAlertController(title: "Zoinks!", message: emojiMessage, preferredStyle: UIAlertController.Style.alert)
 
-        let alertController = UIAlertController(title: "Zoinks!", message: emojis[selectedEmotion!], preferredStyle: UIAlertController.Style.alert)
         
-        alertController.addAction(UIAlertAction(title: "Yea u right", style: UIAlertAction.Style.default, handler: nil))
+        alertController.addAction(UIAlertAction(title: "I ain't mad", style: UIAlertAction.Style.default, handler: nil))
         
         present(alertController, animated: true, completion: nil)
 
