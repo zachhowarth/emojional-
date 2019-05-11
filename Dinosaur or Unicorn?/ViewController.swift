277c515c9ce3c373b9a8bea82a158099f3491cad
Zach Howarth <zachhowarth@gmail.com>
2019-05-11 11:40:57 -0500

    Complete Iteration 2

--- Dinosaur or Unicorn?/ViewController.swift
+++ Dinosaur or Unicorn?/ViewController.swift
@@ -10,18 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let emojis = [ "🦖" : "Ur a heckin' dinosaur" , "🦄" : "Like, ur a fabulous unicorn"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
     @IBAction func showMessage(sender: UIButton) {
-        // in as much detail as possible
-        // pseudo-code the things that need to happen
-        // when a user clicks an emoji button
-        let alertController = UIAlertController(title: "FYI", message: "Ur a heckin' dinosaur brah", preferredStyle: UIAlertController.Style.alert)
         
-        alertController.addAction(UIAlertAction(title: "RAWR!", style: UIAlertAction.Style.default, handler: nil))
+        let selectedEmotion = sender.titleLabel?.text
+        
+        
+
+        let alertController = UIAlertController(title: "Zoinks!", message: emojis[selectedEmotion!], preferredStyle: UIAlertController.Style.alert)
+        
+        alertController.addAction(UIAlertAction(title: "Yea u right", style: UIAlertAction.Style.default, handler: nil))
         
         present(alertController, animated: true, completion: nil)
 
